29d101561ccecb22e8652ae03907d73b1a0459ca
Riccardo Santarossa <brudo1968@libero.it>
2021-10-08 13:30:46 +0200

    RIP

--- src/museo/ThreadVisitatore.java
+++ src/museo/ThreadVisitatore.java
@@ -50,7 +50,7 @@ public class ThreadVisitatore implements Runnable
 			try 
 			{
 				//Thread.sleep(tPermMin + (int) Math.random()*tPermMax);
-				Thread.sleep(5000);
+				Thread.sleep(8000);
 			} 
 			catch (InterruptedException e) {e.printStackTrace();}
 
